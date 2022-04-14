18ee55c56abd89af337192693c666bda1f5f3c34
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-04-14 15:49:32 -0300

    Creating the login component ad its form

--- src/UserList.js
+++ src/UserList.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ListComponent from "./ListComponent";
+import LoginComponent from "./LoginComponent"; 
 
 export default class UserLists extends React.Component{
     state = {lists:[], loading: true}
-
-
 
     async componentDidMount(){
         const config = {
@@ -23,19 +22,19 @@ export default class UserLists extends React.Component{
     render()
     {
         const listApi = this.state.lists;
-        return(
-            <div>
+        var token = '';
 
-              
+        if(token === ''){
+            return <LoginComponent />
+        }
+        else
+            return(
+                <div>
 
-                {listApi.map(list => <ListComponent key={list.id} listName={list.name} items={list.item_set} /> )}
+                    {listApi.map(list => <ListComponent key={list.id} listName={list.name} items={list.item_set} /> )}
 
-                {/* 
-                <ListComponent listName={'Minha lista'} />
-                <ListComponent listName={'Minha lista 2'} />
-                */}
 
-            </div>
+                </div>
         )
     }
     
