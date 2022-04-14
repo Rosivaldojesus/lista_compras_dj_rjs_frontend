a46c28501217d7927c7c84d183f423f3d639e76a
Rosivaldojesus <js.rosivaldo@gmail.com>
2022-04-14 11:47:29 -0300

    Criação do ListComponent

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import ListComponent from './ListComponent';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+
+    <ListComponent />
+
+
   </React.StrictMode>
 );
 
